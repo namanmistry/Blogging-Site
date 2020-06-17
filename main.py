efd7165c224fc23662ba6fee11dc176b98d84063
namanmistry <65106237+namanmistry@users.noreply.github.com>
2020-06-17 12:27:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,45 @@ class blogpost(db.Model):
     like = db.Column(db.Integer, nullable=True)
 
     date = db.Column(db.String(12), nullable=True)
+ 
+class review1(db.Model):
+
+    srno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    message = db.Column(db.String(150), nullable=False)
+    subject = db.Column(db.String(15), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    
+    
 @app.route('/')
 def home():
     posts = blogpost.query.filter_by().all()
     return render_template('index.html',posts=posts)
+
+@app.route('/about')
+def about():
+    reviews = review1.query.order_by(func.rand()).all()
+    return render_template('about.html', reviews=reviews)
+
+@app.route('/review/<string:srno>', methods=['GET'])
+def review_viewer(srno):
+
+    reviews = review1.query.filter_by(srno=srno).first()
+    return render_template('review_viewer.html', reviews=reviews)
+
+@app.route('/submit', methods=['POST', 'GET'])
+def feedback_form():
+
+    name = request.form.get('name')
+    email = request.form.get('email')
+    subject = request.form.get('subject')
+    message = request.form.get('message')
+    entry = review1(name=name, email=email, subject=subject,
+                    message=message, date=datetime.now())
+    db.session.add(entry)
+    db.session.commit()
+    return redirect(url_for('about'))
 
 if __name__=='__main__':
     app.run()
