8e13369e1bd3b80df190c331c679a0ece9cf5f88
namanmistry <65106237+namanmistry@users.noreply.github.com>
2020-06-17 12:52:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,14 @@ class review1(db.Model):
     message = db.Column(db.String(150), nullable=False)
     subject = db.Column(db.String(15), nullable=False)
     date = db.Column(db.String(12), nullable=True)
-    
+ class contact(db.Model):
+
+    srno = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    message = db.Column(db.String(400), nullable=False)
+    subject = db.Column(db.String(205), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
     
 @app.route('/')
 def home():
@@ -68,6 +75,23 @@ def feedback_form():
     db.session.add(entry)
     db.session.commit()
     return redirect(url_for('about'))
+@app.route('/contact', methods=['GET', 'POST'])
+def contact1():
+    if request.method == 'POST':
+        username = request.form.get('name')
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+        entry = contact(username=username, email=email,
+                        subject=subject, message=message, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+        mail.send_message('New message from ' + username,
+                          sender=email,
+                          recipients=["fighter7415963@gmail.com"],
+                          body="subject:" + subject + "\n" + "message:" + message
+                          )
 
+    return render_template('contact.html')
 if __name__=='__main__':
     app.run()
