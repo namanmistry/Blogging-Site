875c92d7be491075e6d490a08b0ed7d81abb43ab
namanmistry <65106237+namanmistry@users.noreply.github.com>
2020-06-17 10:31:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ db = SQLAlchemy(app)
 with open('config.json', 'r') as c:
     params = json.load(c)
 
-app.config['DATABASE_URL'] = params['params']['data-base']
+app.config['DATABASE_URL'] =" postgres://pwmrktgonjcssv:4b833fdabf64cbae502cb725eb6b3a7dfe0e75fd2fc15bf1b330e92d2b52dce3@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/do6ekqf6agn0p"
 class blogpost(db.Model):
 
     srno = db.Column(db.Integer, primary_key=True)
